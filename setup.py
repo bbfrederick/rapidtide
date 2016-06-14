d836fb8213e717a396a3c69b68b0b745c7fdd282
bbfrederick <blaise.frederick@gmail.com>
2016-06-13 23:11:50 -0400

    Put in optional dependency on number

--- setup.py
+++ setup.py
@@ -87,8 +87,7 @@ setup(
     # for example:
     # $ pip install -e .[dev,test]
     extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
+        'jit': ['numba'],
     },
 
     # If there are data files included in your packages that need to be
