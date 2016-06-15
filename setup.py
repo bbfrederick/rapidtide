7e488d48c2b0afa7358c9c332e2713258a830ec5
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-06-15 09:27:39 -0400

    Some patches after first run

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/bin/env python
 """A setuptools based setup module.
 See:
 https://packaging.python.org/en/latest/distributing.html
@@ -17,7 +18,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='sample',
+    name='delaytools',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -80,7 +81,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'sklearn', 'pyqtgraph', 'pyqt', 'nibabel', 'matplotlib'],
+    install_requires=['numpy', 'scipy', 'sklearn', 'pyqtgraph', 'nibabel', 'matplotlib'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
@@ -101,7 +102,7 @@ setup(
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+    #data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
