9b9b7f2eef0728b3990d2150e49775ef43df8119
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-06-16 21:05:31 -0400

    Changed project name to rapidtide

--- rapidtide/version.py
+++ rapidtide/version.py
@@ -25,32 +25,32 @@ CLASSIFIERS = ['Development Status :: 4 - Alpha',
                "Programming Language :: Python"]
 
 # Description should be a one-liner:
-description = "delaytools: a suite of programs for doing RIPTiDe analysis"
+description = "rapidtide: a suite of programs for doing RIPTiDe analysis"
 # Long description will go up on the pypi page
 long_description = """
-Delaytools
+Rapidtide
 ========
-Delaytools is a suite of python programs used to perform time delay analysis 
+Rapidtide is a suite of python programs used to perform time delay analysis 
 on functional imaging data to find time lagged correlations between the 
 voxelwise time series and other time series.
 To get started using these components in your own software, please go to the
 repository README_.
-.. _README: https://github.com/bbfrederick/delaytools/blob/master/README.md
+.. _README: https://github.com/bbfrederick/rapidtide/blob/master/README.md
 License
 =======
-``delaytools`` is licensed under the terms of the Apache 2 license. See the file
+``rapidtide`` is licensed under the terms of the Apache 2 license. See the file
 "LICENSE" for information on the history of this software, terms & conditions
 for usage, and a DISCLAIMER OF ALL WARRANTIES.
 All trademarks referenced herein are property of their respective holders.
 Copyright (c) 2016--, Blaise Frederick, McLean Hospital Brain Imaging Center
 """
 
-NAME = "delaytools"
+NAME = "rapidtide"
 MAINTAINER = "Blaise Frederick"
 MAINTAINER_EMAIL = "blaise.frederick@gmail.com"
 DESCRIPTION = description
 LONG_DESCRIPTION = long_description
-URL = "http://github.com/bbfrederick/delaytools"
+URL = "http://github.com/bbfrederick/rapidtide"
 DOWNLOAD_URL = ""
 LICENSE = "Apache"
 AUTHOR = "Blaise Frederick"
@@ -60,5 +60,5 @@ MAJOR = _version_major
 MINOR = _version_minor
 MICRO = _version_micro
 VERSION = __version__
-PACKAGE_DATA = {'delaytools': [pjoin('data', '*')]}
+PACKAGE_DATA = {'rapidtide': [pjoin('data', '*')]}
 REQUIRES = ["numpy","scipy","matplotlib","sklearn"]
