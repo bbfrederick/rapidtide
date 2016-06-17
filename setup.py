2de0bbfbf2537e311a0798e08b07972d82e45d59
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-06-17 11:08:32 -0400

    Changes to allow code checking

--- setup.py
+++ setup.py
@@ -96,7 +96,7 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'sample': ['package_data.dat'],
+        'example': ['examples/fmri.nii.gz', 'examples/nirs.txt'],
     },
 
     # Although 'package_data' is the preferred approach, in some case you may
