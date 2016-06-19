0f0f7ab40a0cfe46fd13d82e569e36b6129d94ea
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-06-19 10:53:14 -0400

    Attempt 6 at testing

---
+++ scripts/test_correlate.py
@@ -0,0 +1,9 @@
+from tide_funcs import fastcorrelate
+
+sig1 = np.array([0.0,0.0,0,0,1.0,0.0,0,0,0.0])
+sig2 = np.array([0.0,0.0,0,0,1.0,0.0,0,0,0.0])
+
+def testfastcorrelate(sig1, sig2):
+    result = fastcorrelate(sig1,sig2)
+    print(result)
+    assert fastcorrelate(sig1,sig2) == result
