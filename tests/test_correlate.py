a9918a845979cb9b2ce702776571d1f7d046215c
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-08-03 10:38:28 -0400

    Fixing tests

--- tests/test_correlate.py
+++ tests/test_correlate.py
@@ -4,16 +4,13 @@ from __future__ import print_function
 from tide_funcs import fastcorrelate
 import numpy as np
 
-def testfastcorrelate(sig1, sig2):
-    print('entering testfastcorrelate')
+def testfastcorrelate():
+    sig1 = np.array([0.0,0.0,0,0,1.0,0.0,0,0,0.0])
+    sig2 = np.array([0.0,0.0,0,0,1.0,0.0,0,0,0.0])
     assert (fastcorrelate(sig1,sig2) == np.correlate(sig1, sig2, mode='full')).all
 
 def main():
-    print('entering main')
-    sig1 = np.array([0.0,0.0,0,0,1.0,0.0,0,0,0.0])
-    sig2 = np.array([0.0,0.0,0,0,1.0,0.0,0,0,0.0])
-    testfastcorrelate(sig1, sig2)
+    testfastcorrelate()
 
 if __name__ == '__main__':
-    print('starting')
     main()
