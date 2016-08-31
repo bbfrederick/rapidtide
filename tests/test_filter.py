e9bf844271154ecc09feda42766a8a3c8de2ee90
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-08-31 19:20:09 -0400

    Lots of changes in how the testing is done

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,89 +1,163 @@
 #!/usr/bin/env python
 from __future__ import print_function, division
 
-from tide_funcs import noncausalfilter
+from tide_funcs import noncausalfilter, valtoindex
 import numpy as np
+import scipy as sp
 import pylab as plt
 
-def testfilter(display=False):
-    tr = 0.1
-    testlen = 1000
-    freqs = [0.009, 0.1, 0.3, 1.0, 4.0]
-    timeaxis = np.arange(0.0, 1.0 * testlen) * tr
-    overall = 0.0 * timeaxis
-    signals = []
-    for thefreq in freqs:
-        signals.append(0.25 * np.sin(2.0 * np.pi * timeaxis * thefreq))
-        overall += signals[-1]
+def spectralfilterprops(thefilter, debug=False):
+    lowerstop, lowerpass, upperpass, upperstop = thefilter['filter'].getfreqlimits()
+    lowerstopindex = valtoindex(thefilter['frequencies'], lowerstop)
+    lowerpassindex = valtoindex(thefilter['frequencies'], lowerpass, toleft=False)
+    upperpassindex = valtoindex(thefilter['frequencies'], upperpass)
+    upperstopindex = np.min([valtoindex(thefilter['frequencies'], upperstop, toleft=False), len(thefilter['frequencies']) - 1])
+    if debug:
+        print('target freqs:', lowerstop, lowerpass, upperpass, upperstop)
+        print('actual freqs:', thefilter['frequencies'][lowerstopindex],
+                            thefilter['frequencies'][lowerpassindex],
+                            thefilter['frequencies'][upperpassindex],
+                            thefilter['frequencies'][upperstopindex])
+    response = {}
 
-    legend = ['Raw signal']
-        
-    lfoout = [signals[1]]
-    legend.append('LFO source')
-    lfofilter = noncausalfilter(filtertype='lfo',usebutterworth=False, usetrapfftfilt=False)
-    lfoout.append(lfofilter.apply(1.0/tr, overall))
-    legend.append('LFO brickwall')
-    lfofilter = noncausalfilter(filtertype='lfo',usebutterworth=False, usetrapfftfilt=True)
-    lfoout.append(lfofilter.apply(1.0/tr, overall))
-    legend.append('LFO trap FFT')
-    lfofilter = noncausalfilter(filtertype='lfo',usebutterworth=True, butterworthorder=6, usetrapfftfilt=False)
-    lfoout.append(lfofilter.apply(1.0/tr, overall))
-    legend.append('LFO butterworth')
+    passbandmean = np.mean(thefilter['transferfunc'][lowerpassindex:upperpassindex])
+    passbandmax = np.max(thefilter['transferfunc'][lowerpassindex:upperpassindex])
+    passbandmin = np.min(thefilter['transferfunc'][lowerpassindex:upperpassindex])
 
-    respout = [signals[2]]
-    legend.append('Respiratory source')
-    respfilter = noncausalfilter(filtertype='resp',usebutterworth=False, usetrapfftfilt=False)
-    respout.append(respfilter.apply(1.0/tr, overall))
-    legend.append('Respiratory brickwall')
-    respfilter = noncausalfilter(filtertype='resp',usebutterworth=False, usetrapfftfilt=True)
-    respout.append(respfilter.apply(1.0/tr, overall))
-    legend.append('Respiratory trap FFT')
-    respfilter = noncausalfilter(filtertype='resp',usebutterworth=True, butterworthorder=6, usetrapfftfilt=False)
-    respout.append(respfilter.apply(1.0/tr, overall))
-    legend.append('Respiratory butterworth')
+    response['passbandripple'] = (passbandmax - passbandmin)/passbandmean
 
-    cardout = [signals[3]]
-    legend.append('Cardiac source')
-    cardfilter = noncausalfilter(filtertype='cardiac',usebutterworth=False, usetrapfftfilt=False)
-    cardout.append(cardfilter.apply(1.0/tr, overall))
-    legend.append('Cardiac brickwall')
-    cardfilter = noncausalfilter(filtertype='cardiac',usebutterworth=False, usetrapfftfilt=True)
-    cardout.append(cardfilter.apply(1.0/tr, overall))
-    legend.append('Cardiac trap FFT')
-    cardfilter = noncausalfilter(filtertype='cardiac',usebutterworth=True, butterworthorder=6, usetrapfftfilt=False)
-    cardout.append(cardfilter.apply(1.0/tr, overall))
-    legend.append('Cardiac butterworth')
+    if lowerstopindex > 2:
+        response['lowerstopmean'] = np.mean(thefilter['transferfunc'][0:lowerstopindex])/passbandmean
+        response['lowerstopmax'] = np.max(np.abs(thefilter['transferfunc'][0:lowerstopindex]))/passbandmean
+    else:
+        response['lowerstopmean'] = 0.0
+        response['lowerstopmax'] = 0.0
 
+    if len(thefilter['transferfunc']) - upperstopindex > 2:
+        response['upperstopmean'] = np.mean(thefilter['transferfunc'][upperstopindex:-1])/passbandmean
+        response['upperstopmax'] = np.max(np.abs(thefilter['transferfunc'][upperstopindex:-1]))/passbandmean
+    else:
+        response['upperstopmean'] = 0.0
+        response['upperstopmax'] = 0.0
+    return response
+
+
+def eval_filterprops(sampletime=0.72, tclengthinsecs=300.0, numruns=100, display=False):
+    tclen = int(tclengthinsecs // sampletime)
+    print('Testing transfer function:')
+    lowestfreq = 1.0/(sampletime * tclen)
+    nyquist = 0.5/sampletime
+    print('    sampletime=',sampletime,', timecourse length=',tclengthinsecs, 's,  possible frequency range:',lowestfreq, nyquist)
+    timeaxis = np.arange(0.0, 1.0 * tclen) * sampletime
+
+    overall = np.random.normal(size=tclen)
+    nperseg = np.min([tclen, 256])
+    f, dummy = sp.signal.welch(overall, fs=1.0/sampletime, nperseg=nperseg)
+
+    allfilters = []
+
+    # construct all the filters
+    for filtertype in ['lfo', 'resp', 'cardiac']:
+        testfilter = noncausalfilter(filtertype=filtertype)
+        lstest, lptest, uptest, ustest = testfilter.getfreqlimits()
+        if lptest < nyquist:
+            allfilters.append(
+                {
+                    'name': filtertype + ' brickwall',
+                    'filter': noncausalfilter(filtertype=filtertype),
+                })
+            allfilters.append(
+                {
+                    'name': filtertype + ' trapezoidal',
+                    'filter': noncausalfilter(filtertype=filtertype, usetrapfftfilt=True),
+                })
+
+    # calculate the transfer functions for the filters
+    for index in range(0, len(allfilters)):
+        psd_raw = 0.0 * dummy
+        psd_filt = 0.0 * dummy
+        for i in range(0,numruns):
+            inputsig = np.random.normal(size=tclen)
+            outputsig = allfilters[index]['filter'].apply(1.0/sampletime, inputsig)
+            f, raw = sp.signal.welch(inputsig, fs=1.0/sampletime, nperseg=nperseg)
+            f, filt = sp.signal.welch(outputsig, fs=1.0/sampletime, nperseg=nperseg)
+            psd_raw += raw
+            psd_filt += filt
+        allfilters[index]['frequencies'] = f
+        allfilters[index]['transferfunc'] = psd_filt / psd_raw
+
+    # show transfer functions
     if display:
+        legend = []
         plt.figure()
-        plt.ylim([-1.0, 5.0])
-        plt.plot(overall)
         plt.hold(True)
-        for tc in lfoout:
-            plt.plot(tc + 1.0)
-        for tc in respout:
-            plt.plot(tc + 2.0)
-        for tc in cardout:
-            plt.plot(tc + 3.0)
+        plt.ylim([-1.1, 1.1 * len(allfilters)])
+        offset = 0.0
+        for thefilter in allfilters:
+            plt.plot(thefilter['frequencies'], thefilter['transferfunc'] + offset)
+            legend.append(thefilter['name'])
+            offset += 1.1
         plt.legend(legend)
         plt.show()
 
-    aethresh = 1 
-    for i in range(1,4):
-        for j in range(0, len(cardout[0])):
-            print(cardout[0][j], cardout[1][j], cardout[2][j], cardout[3][j])
-        np.testing.assert_almost_equal(cardout[0], cardout[i], aethresh)
-   
-        for j in range(0, len(respout[0])):
-            print(respout[0][j], respout[1][j], respout[2][j], respout[3][j])
-        np.testing.assert_almost_equal(respout[0], respout[i], aethresh)
+    # test transfer function responses
+    for thefilter in allfilters:
+        response = spectralfilterprops(thefilter)
+        print('    Evaluating', thefilter['name'], 'transfer function')
+        assert response['passbandripple'] < 0.4
+        assert response['lowerstopmax'] < 1e4
+        assert response['lowerstopmean'] < 1e4
+        assert response['upperstopmax'] < 1e4
+        assert response['upperstopmean'] < 1e4
 
-        for j in range(0, len(lfoout[0])):
-            print(lfoout[0][j], lfoout[1][j], lfoout[2][j], lfoout[3][j])
-        np.testing.assert_almost_equal(lfoout[0], lfoout[i], aethresh)
+    # construct some test waveforms for end effects
+    testwaves = []
+    testwaves.append({
+        'name':        'constant high',
+        'timeaxis':    1.0 * timeaxis,
+        'waveform':    np.ones((tclen), dtype='float'),
+        })
+    testwaves.append({
+        'name':        'white noise',
+        'timeaxis':    1.0 * timeaxis,
+        'waveform':    0.3 * np.random.normal(size=tclen),
+        })
+    
+    scratch = timeaxis * 0.0
+    scratch[int(tclen / 5):int(2 * tclen / 5)] = 1.0
+    scratch[int(3 * tclen / 5):int(4 * tclen / 5)] = 1.0
+    testwaves.append({
+        'name':        'block regressor',
+        'timeaxis':    1.0 * timeaxis,
+        'waveform':    1.0 * scratch,
+        })
+
+    # show the end effects waveforms
+    if display:
+        legend = []
+        plt.figure()
+        plt.hold(True)
+        plt.ylim([-2.2, 2.2 * len(testwaves)])
+        offset = 0.0
+        for thewave in testwaves:
+            for thefilter in allfilters:
+                plt.plot(thewave['timeaxis'], offset + thefilter['filter'].apply(1.0/sampletime, thewave['waveform']))
+                legend.append(thewave['name'] + ': '+ thefilter['name'])
+                offset += 1.1
+            #plt.plot(thewave['timeaxis'], thewave['waveform'] + offset)
+            #legend.append(thewave['name'])
+            #offset += 2.2
+            plt.legend(legend)
+            plt.show()
+
+
+def test_filterprops(display=False):
+    eval_filterprops(sampletime=0.72, tclengthinsecs=300.0, numruns=100, display=display)
+    eval_filterprops(sampletime=2.0,  tclengthinsecs=300.0, numruns=100, display=display)
+    eval_filterprops(sampletime=0.1,  tclengthinsecs=1000.0, numruns=10, display=display)
 
 def main():
-    testfilter(display=True)
+    test_filterprops(display=True)
 
 if __name__ == '__main__':
     main()
