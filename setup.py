980bca77119ee03f686d7f9cd32e53b4688f5568
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2016-12-09 13:44:33 -0500

    Fixed scikit-learn requrement

--- setup.py
+++ setup.py
@@ -145,7 +145,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'pandas', 'sklearn', 'nibabel', 'matplotlib', 'statsmodels'],
+    install_requires=['numpy', 'scipy', 'pandas', 'scikit-learn', 'nibabel', 'matplotlib', 'statsmodels'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
