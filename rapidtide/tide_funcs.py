1797de5e699ae240ecf48db23480957b1b752f12
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2018-06-28 11:06:34 -0400

    Pulled IO functions out of tide_funcs into a new module

--- rapidtide/tide_funcs.py
+++ rapidtide/tide_funcs.py
@@ -37,6 +37,7 @@ import resource
 #from scipy import signal
 from scipy.stats import johnsonsb
 
+import rapidtide.io_funcs as tide_io
 
 # ---------------------------------------- Global constants -------------------------------------------
 defaultbutterorder = 6
@@ -157,110 +158,6 @@ def version():
         return 'UNKNOWN', 'UNKNOWN'
 
 
-# ---------------------------------------- NIFTI file manipulation ---------------------------
-if nibabelexists:
-    def readfromnifti(inputfile):
-        if os.path.isfile(inputfile):
-            inputfilename = inputfile 
-        elif os.path.isfile(inputfile + '.nii.gz'):
-            inputfilename = inputfile  + '.nii.gz'
-        elif os.path.isfile(inputfile + '.nii'):
-            inputfilename = inputfile + '.nii'
-        else:
-            print('nifti file', inputfile, 'does not exist')
-            sys.exit()
-        nim = nib.load(inputfilename)
-        nim_data = nim.get_data()
-        nim_hdr = nim.get_header()
-        thedims = nim_hdr['dim'].copy()
-        thesizes = nim_hdr['pixdim'].copy()
-        return nim, nim_data, nim_hdr, thedims, thesizes
-
-
-    # dims are the array dimensions along each axis
-    def parseniftidims(thedims):
-        return thedims[1], thedims[2], thedims[3], thedims[4]
-
-
-    # sizes are the mapping between voxels and physical coordinates
-    def parseniftisizes(thesizes):
-        return thesizes[1], thesizes[2], thesizes[3], thesizes[4]
-
-
-    def savetonifti(thearray, theheader, thepixdim, thename):
-        outputaffine = theheader.get_best_affine()
-        qaffine, qcode = theheader.get_qform(coded=True)
-        saffine, scode = theheader.get_sform(coded=True)
-        if theheader['magic'] == 'n+2':
-            output_nifti = nib.Nifti2Image(thearray, outputaffine, header=theheader)
-            suffix = '.nii'
-        else:
-            output_nifti = nib.Nifti1Image(thearray, outputaffine, header=theheader)
-            suffix = '.nii.gz'
-        output_nifti.set_qform(qaffine, code=int(qcode))
-        output_nifti.set_sform(saffine, code=int(scode))
-        output_nifti.to_filename(thename + suffix)
-        output_nifti = None
-
-
-    def checkifnifti(filename):
-        if filename.endswith(".nii") or filename.endswith(".nii.gz"):
-            return True
-        else:
-            return False
-
-
-    def checkiftext(filename):
-        if filename.endswith(".txt"):
-            return True
-        else:
-            return False
-
-
-    def getniftiroot(filename):
-        if filename.endswith(".nii"):
-            return filename[:-4]
-        elif filename.endswith(".nii.gz"):
-            return filename[:-7]
-        else:
-            return filename
-
-
-    def fmritimeinfo(niftifilename):
-        nim = nib.load(niftifilename)
-        hdr = nim.get_header()
-        thedims = hdr['dim']
-        thesizes = hdr['pixdim']
-        if hdr.get_xyzt_units()[1] == 'msec':
-            tr = thesizes[4] / 1000.0
-        else:
-            tr = thesizes[4]
-        timepoints = thedims[4]
-        return tr, timepoints
-
-
-    def checkspacematch(dims1, dims2):
-        for i in range(1, 4):
-            if dims1[i] != dims2[i]:
-                print("File spatial voxels do not match")
-                print("dimension ", i, ":", dims1[i], "!=", dims2[i])
-                return False
-            else:
-                return True
-
-
-    def checktimematch(dims1, dims2, numskip1, numskip2):
-        if (dims1[4] - numskip1) != (dims2[4] - numskip2):
-            print("File numbers of timepoints do not match")
-            print("dimension ", 4, ":", dims1[4],
-                  "(skip ", numskip1, ") !=",
-                  dims2[4],
-                  " (skip ", numskip2, ")")
-            return False
-        else:
-            return True
-
-
 # --------------------------- timing functions -------------------------------------------------
 def timefmt(thenumber):
     return "{:10.2f}".format(thenumber)
@@ -288,7 +185,7 @@ def proctiminginfo(thetimings, outputfile='', extraheader=None):
         theinfolist.append(outstring)
         lasteventtime = float(theevent[1])
     if outputfile != '':
-        writevec(theinfolist, outputfile)
+        tide_io.writevec(theinfolist, outputfile)
 
 
 # --------------------------- histogram functions -------------------------------------------------
@@ -339,8 +236,8 @@ def makeandsavehistogram(indata, histlen, endtrim, outname,
     peakwidth = (thestore[0, peakindex + numbins] - thestore[0, peakindex]) * 2.0
     if refine:
         peakheight, peaklag, peakwidth = gaussfit(peakheight, peaklag, peakwidth, thestore[0, :], thestore[1, :])
-    writenpvecs(np.array([peaklag]), outname + '_peak.txt')
-    writenpvecs(thestore, outname + '.txt')
+    tide_io.writenpvecs(np.array([peaklag]), outname + '_peak.txt')
+    tide_io.writenpvecs(thestore, outname + '.txt')
     if displayplots:
         fig = pl.figure()
         ax = fig.add_subplot(111)
@@ -415,7 +312,7 @@ def sigFromDistributionData(vallist, histlen, thepercentiles, displayplots=False
 
 
 def rfromp(fitfile, thepercentiles, numbins=1000):
-    thefit = np.array(readvecs(fitfile)[0]).astype('float64')
+    thefit = np.array(tide_io.readvecs(fitfile)[0]).astype('float64')
     print('thefit = ', thefit)
     return getfracvalsfromfit(thefit, thepercentiles, numbins=1000, displayplots=True)
 
@@ -507,171 +404,6 @@ def mlregress(x, y, intercept=True):
 
     return np.atleast_1d(solution[0].T), R
 
-
-# --------------------------- non-NIFTI file I/O functions ------------------------------------------
-def checkifparfile(filename):
-    if filename.endswith(".par"):
-        return True
-    else:
-        return False
-
-
-def readbidssidecar(inputfilename):
-    thefileroot, theext = os.path.splitext(inputfilename)
-    if os.path.exists(thefileroot + '.json'):
-        with open(thefileroot + '.json', 'r') as json_data:
-            d = json.load(json_data)
-            return d
-    else:
-        print('sidecar file does not exist')
-        return {}
-
-
-def readbidstsv(inputfilename):
-    thefileroot, theext = os.path.splitext(inputfilename)
-    if os.path.exists(thefileroot + '.json') and os.path.exists(thefileroot + '.tsv.gz'):
-        with open(thefileroot + '.json', 'r') as json_data:
-            d = json.load(json_data)
-            try:
-                samplerate = float(d['SamplingFrequency'])
-            except:
-                print('no samplerate found in json')
-                return [None, None, None, None]
-            try:
-                starttime = float(d['StartTime'])
-            except:
-                print('no starttime found in json')
-                return [None, None, None, None]
-            try:
-                columns = d['Columns']
-            except:
-                print('no columns found in json')
-                return [None, None, None, None]
-        df = pd.read_csv(thefileroot + '.tsv.gz', compression='gzip', header=0, sep='\t', quotechar='"')
-        return samplerate, starttime, columns, np.transpose(df.as_matrix())
-    else:
-        print('file pair does not exist')
-        return [None, None, None, None]
-
-
-def readcolfrombidstsv(inputfilename, columnnum=0, columnname=None):
-    samplerate, starttime, columns, data = readbidstsv(inputfilename)
-    if data is None:
-        print('no valid datafile found')
-        return None, None, None
-    else:
-        if columnname is not None:
-            # looking for a named column
-            try:
-                thecolnum = columns.index(columnname)
-                return samplerate, starttime, data[thecolnum, :]
-            except:
-                print('no column named', columnname, 'in', inputfilename)
-                return None, None, None
-        # we can only get here if columnname is undefined
-        if not (0 < columnum < len(columns)):
-            print('specified column number', columnnum, 'is out of range in', inputfilename)
-            return None, None, None
-        else:
-            return samplerate, starttime, data[thecolnum, :]
-        
-    
-def readvecs(inputfilename):
-    thefile = open(inputfilename, 'r')
-    lines = thefile.readlines()
-    numvecs = len(lines[0].split())
-    inputvec = np.zeros((numvecs, MAXLINES), dtype='float64')
-    numvals = 0
-    for line in lines:
-        if len(line) > 1:
-            numvals += 1
-            thetokens = line.split()
-            for vecnum in range(0, numvecs):
-                inputvec[vecnum, numvals - 1] = np.float64(thetokens[vecnum])
-    return 1.0 * inputvec[:, 0:numvals]
-
-
-def readvec(inputfilename):
-    inputvec = np.zeros(MAXLINES, dtype='float64')
-    numvals = 0
-    with open(inputfilename, 'r') as thefile:
-        lines = thefile.readlines()
-        for line in lines:
-            if len(line) > 1:
-                numvals += 1
-                inputvec[numvals - 1] = np.float64(line)
-    return 1.0 * inputvec[0:numvals]
-
-
-def readlabels(inputfilename):
-    inputvec = []
-    with open(inputfilename, 'r') as thefile:
-        lines = thefile.readlines()
-        for line in lines:
-            inputvec.append(line.rstrip())
-    return inputvec
-
-
-def writedict(thedict, outputfile, lineend=''):
-    if lineend == 'mac':
-        thelineending = '\r'
-        openmode = 'wb'
-    elif lineend == 'win':
-        thelineending = '\r\n'
-        openmode = 'wb'
-    elif lineend == 'linux':
-        thelineending = '\n'
-        openmode = 'wb'
-    else:
-        thelineending = '\n'
-        openmode = 'w'
-    with open(outputfile, openmode) as FILE:
-        for key, value in sorted(thedict.items()):
-            FILE.writelines(str(key) + ':\t' + str(value) + thelineending)
-
-
-def writevec(thevec, outputfile, lineend=''):
-    if lineend == 'mac':
-        thelineending = '\r'
-        openmode = 'wb'
-    elif lineend == 'win':
-        thelineending = '\r\n'
-        openmode = 'wb'
-    elif lineend == 'linux':
-        thelineending = '\n'
-        openmode = 'wb'
-    else:
-        thelineending = '\n'
-        openmode = 'w'
-    with open(outputfile, openmode) as FILE:
-        for i in thevec:
-            FILE.writelines(str(i) + thelineending)
-
-
-# rewritten to guarantee file closure, combines writenpvec and writenpvecs
-def writenpvecs(thevecs, outputfile, lineend=''):
-    theshape = np.shape(thevecs)
-    if lineend == 'mac':
-        thelineending = '\r'
-        openmode = 'wb'
-    elif lineend == 'win':
-        thelineending = '\r\n'
-        openmode = 'wb'
-    elif lineend == 'linux':
-        thelineending = '\n'
-        openmode = 'wb'
-    else:
-        thelineending = '\n'
-        openmode = 'w'
-    with open(outputfile, openmode) as FILE:
-        if thevecs.ndim == 2:
-            for i in range(0, theshape[1]):
-                for j in range(0, theshape[0]):
-                    FILE.writelines(str(thevecs[j, i]) + '\t')
-                FILE.writelines(thelineending)
-        else:
-            for i in range(0, theshape[0]):
-                FILE.writelines(str(thevecs[i]) + thelineending)
 
 
 # --------------------------- correlation functions -------------------------------------------------
@@ -3062,7 +2794,7 @@ def isexecutable(command):
 
 
 def savecommandline(theargs, thename):
-    writevec([' '.join(theargs)], thename + '_commandline.txt')
+    tide_io.writevec([' '.join(theargs)], thename + '_commandline.txt')
 
 
 def valtoindex(thearray, thevalue, toleft=True):
