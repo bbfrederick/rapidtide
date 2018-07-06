8c39d0787a09043aacd7387f01dee8b84d85ea50
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2018-07-06 13:04:59 -0400

    Factored tide_funcs routines into util

---
+++ rapidtide/util.py
@@ -0,0 +1,290 @@
+#!/usr/bin/env python
+#
+#   Copyright 2016 Blaise Frederick
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# $Author: frederic $
+# $Date: 2016/07/12 13:50:29 $
+# $Id: tide_funcs.py,v 1.4 2016/07/12 13:50:29 frederic Exp $
+#
+from __future__ import print_function, division
+
+import numpy as np
+import scipy as sp
+from scipy import fftpack, ndimage, signal
+from numpy.fft import rfftn, irfftn
+import warnings
+import time
+import sys
+import bisect
+import os
+import resource
+
+#from scipy import signal
+from scipy.stats import johnsonsb
+
+import rapidtide.io as tide_io
+import rapidtide.filter as tide_filt
+import rapidtide.resample as tide_resample
+
+# ---------------------------------------- Global constants -------------------------------------------
+defaultbutterorder = 6
+MAXLINES = 10000000
+donotbeaggressive = True
+
+# ----------------------------------------- Conditional imports ---------------------------------------
+try:
+    from memory_profiler import profile
+
+    memprofilerexists = True
+except ImportError:
+    memprofilerexists = False
+
+
+try:
+    from numba import jit
+
+    numbaexists = True
+except ImportError:
+    numbaexists = False
+
+
+try:
+    import nibabel as nib
+
+    nibabelexists = True
+except ImportError:
+    nibabelexists = False
+
+
+donotusenumba = False
+
+
+try:
+    import pyfftw
+    pyfftwexists = True
+    fftpack = pyfftw.interfaces.scipy_fftpack
+    pyfftw.interfaces.cache.enable()
+except ImportError:
+    pyfftwexists = False
+
+
+def checkimports(optiondict):
+    if pyfftwexists:
+        print('monkey patched scipy.fftpack to use pyfftw')
+    else:
+        print('using standard scipy.fftpack')
+    optiondict['pyfftwexists'] = pyfftwexists
+
+    if numbaexists:
+        print('numba exists')
+    else:
+        print('numba does not exist')
+    optiondict['numbaexists'] = numbaexists
+
+    if memprofilerexists:
+        print('memprofiler exists')
+    else:
+        print('memprofiler does not exist')
+    optiondict['memprofilerexists'] = memprofilerexists
+
+    if nibabelexists:
+        print('nibabel exists')
+    else:
+        print('nibabel does not exist')
+    optiondict['nibabelexists'] = nibabelexists
+
+    if donotbeaggressive:
+        print('no aggressive optimization')
+    else:
+        print('aggressive optimization')
+    optiondict['donotbeaggressive'] = donotbeaggressive
+
+    global donotusenumba
+    if donotusenumba:
+        print('will not use numba even if present')
+    else:
+        print('using numba if present')
+    optiondict['donotusenumba'] = donotusenumba
+
+
+def conditionaljit():
+    def resdec(f):
+        if (not numbaexists) or donotusenumba:
+            return f
+        return jit(f)
+
+    return resdec
+
+
+def conditionaljit2():
+    def resdec(f):
+        if (not numbaexists) or donotusenumba or donotbeaggressive:
+            return f
+        return jit(f)
+
+    return resdec
+
+
+def disablenumba():
+    global donotusenumba
+    donotusenumba = True
+
+
+# --------------------------- Utility functions -------------------------------------------------
+def logmem(msg, file=None):
+    global lastmaxrss_parent, lastmaxrss_child
+    if msg is None:
+        lastmaxrss_parent = 0
+        lastmaxrss_child = 0
+        logline = ','.join([ 
+            '',
+            'Self Max RSS',
+            'Self Diff RSS',
+            'Self Shared Mem',
+            'Self Unshared Mem',
+            'Self Unshared Stack',
+            'Self Non IO Page Fault'
+            'Self IO Page Fault'
+            'Self Swap Out',
+            'Children Max RSS',
+            'Children Diff RSS',
+            'Children Shared Mem',
+            'Children Unshared Mem',
+            'Children Unshared Stack',
+            'Children Non IO Page Fault'
+            'Children IO Page Fault'
+            'Children Swap Out'])
+    else:
+        rcusage = resource.getrusage(resource.RUSAGE_SELF)
+        outvals = [msg]
+        outvals.append(str(rcusage.ru_maxrss))
+        outvals.append(str(rcusage.ru_maxrss - lastmaxrss_parent))
+        lastmaxrss_parent = rcusage.ru_maxrss
+        outvals.append(str(rcusage.ru_ixrss))
+        outvals.append(str(rcusage.ru_idrss))
+        outvals.append(str(rcusage.ru_isrss))
+        outvals.append(str(rcusage.ru_minflt))
+        outvals.append(str(rcusage.ru_majflt))
+        outvals.append(str(rcusage.ru_nswap))
+        rcusage = resource.getrusage(resource.RUSAGE_CHILDREN)
+        outvals.append(str(rcusage.ru_maxrss))
+        outvals.append(str(rcusage.ru_maxrss - lastmaxrss_child))
+        lastmaxrss_child = rcusage.ru_maxrss
+        outvals.append(str(rcusage.ru_ixrss))
+        outvals.append(str(rcusage.ru_idrss))
+        outvals.append(str(rcusage.ru_isrss))
+        outvals.append(str(rcusage.ru_minflt))
+        outvals.append(str(rcusage.ru_majflt))
+        outvals.append(str(rcusage.ru_nswap))
+        logline = ','.join(outvals)
+    if file is None:
+        print(logline)
+    else:
+        file.writelines(logline + "\n")
+
+
+def findexecutable(command):
+    import shutil
+
+    theversion = sys.version_info
+    if (theversion[0] >= 3) and (theversion[1] >= 3):
+        return shutil.which(command)
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            if os.access(os.path.join(path, command), os.X_OK):
+                return os.path.join(path, command)
+        return None
+
+
+def isexecutable(command):
+    import shutil
+
+    theversion = sys.version_info
+    if (theversion[0] >= 3) and (theversion[1] >= 3):
+        if shutil.which(command) is not None:
+            return True
+        else:
+            return False
+    else:
+        return any(
+            os.access(os.path.join(path, command), os.X_OK) 
+            for path in os.environ["PATH"].split(os.pathsep)
+        )
+
+
+def savecommandline(theargs, thename):
+    tide_io.writevec([' '.join(theargs)], thename + '_commandline.txt')
+
+
+def valtoindex(thearray, thevalue, toleft=True):
+    if toleft:
+        return bisect.bisect_left(thearray, thevalue)
+    else:
+        return bisect.bisect_right(thearray, thevalue)
+
+
+def progressbar(thisval, end_val, label='Percent', barsize=60):
+    percent = float(thisval) / end_val
+    hashes = '#' * int(round(percent * barsize))
+    spaces = ' ' * (barsize - len(hashes))
+    sys.stdout.write("\r{0}: [{1}] {2:.3f}%".format(label, hashes + spaces, 100.0 * percent))
+    sys.stdout.flush()
+
+
+# ------------------------------------------ Version function ----------------------------------
+def version():
+    thispath, thisfile = os.path.split(__file__)
+    print(thispath)
+    if os.path.isfile(os.path.join(thispath, '_gittag.py')):
+        with open(os.path.join(thispath, '_gittag.py')) as f:
+            for line in f:
+                if line.startswith('__gittag__'):
+                    fulltag = (line.split()[2]).split('-')
+                    break
+        return fulltag[0][1:], '-'.join(fulltag[1:])[:-1]
+    else:
+        return 'UNKNOWN', 'UNKNOWN'
+
+
+# --------------------------- timing functions -------------------------------------------------
+def timefmt(thenumber):
+    return "{:10.2f}".format(thenumber)
+
+
+def proctiminginfo(thetimings, outputfile='', extraheader=None):
+    theinfolist = []
+    start = thetimings[0]
+    starttime = float(start[1])
+    lasteventtime = starttime
+    if extraheader is not None:
+        print(extraheader)
+        theinfolist.append(extraheader)
+    headerstring = 'Clock time\tProgram time\tDuration\tDescription'
+    print(headerstring)
+    theinfolist.append(headerstring)
+    for theevent in thetimings:
+        theduration = float(theevent[1] - lasteventtime)
+        outstring = time.strftime("%Y%m%dT%H%M%S", time.localtime(theevent[1])) + \
+                    timefmt(float(theevent[1]) - starttime) + \
+                    '\t' + timefmt(theduration) + '\t' + theevent[0]
+        if theevent[2] is not None:
+            outstring += " ({0:.2f} {1}/second)".format(float(theevent[2]) / theduration, theevent[3])
+        print(outstring)
+        theinfolist.append(outstring)
+        lasteventtime = float(theevent[1])
+    if outputfile != '':
+        tide_io.writevec(theinfolist, outputfile)
+
+
