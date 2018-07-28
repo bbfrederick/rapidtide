a3b113d0a60b5e8a7cd0f4df28c3030bb517fce4
Taylor Salo <tsalo006@fiu.edu>
2018-07-27 21:08:06 -0400

    Add showxcorrx workflow and examples gallery.

---
+++ examples/plot_showxcorrx.py
@@ -0,0 +1,55 @@
+# emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; indent-tabs-mode: nil -*-
+# ex: set sts=4 ts=4 sw=4 et:
+"""
+.. _ex1:
+
+=========================================
+ Run showxcorrx workflow
+=========================================
+
+Runs a workflow.
+
+"""
+# sphinx_gallery_thumbnail_number = 2
+
+###############################################################################
+# Start with the necessary imports
+# --------------------------------
+from os import remove
+import matplotlib.pyplot as plt
+import numpy as np
+from rapidtide.workflows import showxcorrx_workflow
+
+###############################################################################
+# Arguments
+# --------------------------------
+np.random.seed(314)
+dat1 = np.random.random(500)
+f1 = 'f1.txt'
+np.savetxt(f1, dat1)
+
+np.random.seed(42)
+dat2 = np.random.random(500)
+f2 = 'f2.txt'
+np.savetxt(f2, dat2)
+
+samplerate = 2.
+
+###############################################################################
+# Let's plot the timeseries first
+# --------------------------------
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(dat1, color='red', alpha=0.7)
+ax.plot(dat2, color='blue', alpha=0.7)
+fig.show()
+
+###############################################################################
+# Now let's run it
+# --------------------------------
+showxcorrx_workflow(f1, f2, samplerate)
+
+###############################################################################
+# Clean up
+# --------------------------------
+remove(f1)
+remove(f2)
---
+++ rapidtide/workflows/parser_funcs.py
@@ -0,0 +1,54 @@
+"""
+Functions for parsers.
+"""
+import os.path as op
+
+
+def is_valid_file(parser, arg):
+    """
+    Check if argument is existing file.
+    """
+    if not op.isfile(arg) and arg is not None:
+        parser.error('The file {0} does not exist!'.format(arg))
+
+    return arg
+
+
+def invert_float(parser, arg):
+    """
+    Check if argument is float or auto.
+    """
+    if arg != 'auto':
+        try:
+            arg = float(arg)
+        except parser.error:
+            parser.error('Value {0} is not a float or "auto"'.format(arg))
+
+    if arg != 'auto':
+        arg = 1. / arg
+    return arg
+
+
+def is_float(parser, arg):
+    """
+    Check if argument is float or auto.
+    """
+    if arg != 'auto':
+        try:
+            arg = float(arg)
+        except parser.error:
+            parser.error('Value {0} is not a float or "auto"'.format(arg))
+
+    return arg
+
+
+def is_range(parser, arg):
+    """
+    Check if argument is min/max pair.
+    """
+    if arg is not None and len(arg) != 2:
+        parser.error('Argument must be min/max pair.')
+    elif arg is not None and arg[0] > arg[1]:
+        parser.error('Argument min must be lower than max.')
+
+    return arg
