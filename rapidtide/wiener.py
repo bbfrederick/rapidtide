Reformat and correct function calls, properly pass numerical precision args

rapidtide/wiener.py
@@ -31,7 +31,11 @@ import rapidtide.multiproc as tide_multiproc
 import rapidtide.util as tide_util
 
 
-def _procOneVoxelWiener(vox, lagtc, inittc, rt_floatset=np.float64, rt_floattype='float64'):
+def _procOneVoxelWiener(vox,
+                        lagtc,
+                        inittc,
+                        rt_floatset=np.float64,
+                        rt_floattype='float64'):
     thefit, R = tide_fit.mlregress(lagtc, inittc)
     fitcoff = rt_floatset(thefit[0, 1])
     datatoremove = rt_floatset(fitcoff * lagtc)
@@ -39,8 +43,21 @@ def _procOneVoxelWiener(vox, lagtc, inittc, rt_floatset=np.float64, rt_floattype
            rt_floatset(thefit[0, 1] / thefit[0, 0]), datatoremove, rt_floatset(inittc - datatoremove)
 
 
-def wienerpass(numspatiallocs, reportstep, fmri_data, threshval, lagtc, optiondict, meanvalue, rvalue, r2value, fitcoff,
-               fitNorm, datatoremove, filtereddata):
+def wienerpass(numspatiallocs,
+               reportstep,
+               fmri_data,
+               threshval,
+               lagtc,
+               optiondict,
+               meanvalue,
+               rvalue,
+               r2value,
+               fitcoff,
+               fitNorm,
+               datatoremove,
+               filtereddata,
+               rt_floatset=np.float64,
+               rt_floattype='float64'):
     inputshape = np.shape(fmri_data)
     themask = np.where(np.mean(fmri_data, axis=1) > threshval, 1, 0)
     if optiondict['nprocs'] > 1:
@@ -60,38 +77,12 @@ def wienerpass(numspatiallocs, reportstep, fmri_data, threshval, lagtc, optiondi
                                                  lagtc[val, :],
                                                  fmri_data[val,
                                                  optiondict['addedskip']:],
-                                                 rt_floatset=np.float64,
-                                                 rt_floattype='float64'))
+                                                 rt_floatset=rt_floatset,
+                                                 rt_floattype=rt_floattype))
 
                 except Exception as e:
                     print("error!", e)
                     break
-
-        """"
-        # initialize the workers and the queues
-        n_workers = optiondict['nprocs']
-        inQ = mp.Queue()
-        outQ = mp.Queue()
-        workers = [mp.Process(target=Wiener_consumer, args=(inQ, outQ)) for i in range(n_workers)]
-        for i, w in enumerate(workers):
-            w.start()
-
-        # pack the data and send to workers
-        data_in = []
-        for d in range(numspatiallocs):
-            if np.mean(fmri_data[d, optiondict['addedskip']:]) >= threshval:
-                data_in.append(d)
-        print('processing', len(data_in), 'voxels with', n_workers, 'processes')
-        data_out = tide_multiproc._process_data(data_in, inQ, outQ, showprogressbar=optiondict['showprogressbar'],
-                                chunksize=optiondict['mp_chunksize'])
-
-        # shut down workers
-        for i in range(n_workers):
-            inQ.put(None)
-        for w in workers:
-            w.terminate()
-            w.join()
-        """
 
         data_out = tide_multiproc.run_multiproc(Wiener_consumer,
                                                 inputshape, themask,
@@ -118,8 +109,11 @@ def wienerpass(numspatiallocs, reportstep, fmri_data, threshval, lagtc, optiondi
             inittc = fmri_data[vox, optiondict['addedskip']:].copy()
             if np.mean(inittc) >= threshval:
                 dummy, meanvalue[vox], rvalue[vox], r2value[vox], fitcoff[vox], fitNorm[vox], datatoremove[vox], \
-                filtereddata[vox] = _procOneVoxelWiener(vox, lagtc[vox, :], inittc, rt_floatset=np.float64,
-                                                        rt_floattype='float64')
+                filtereddata[vox] = _procOneVoxelWiener(vox,
+                                                        lagtc[vox, :],
+                                                        inittc,
+                                                        rt_floatset=rt_floatset,
+                                                        t_floattype=rt_floattype)
             volumetotal += 1
 
     return volumetotal