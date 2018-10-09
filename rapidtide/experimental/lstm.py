ac9b93522d0f51a67d52a9369e246db055b65431
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2018-10-09 11:11:21 -0400

    Various unsuccessful efforts to get this working

--- rapidtide/experimental/lstm.py
+++ rapidtide/experimental/lstm.py
@@ -12,34 +12,50 @@ import numpy as np
 
 from keras.models import Sequential
 from keras.optimizers import RMSprop
-from keras.layers import Bidirectional, Convolution1D, Dense, Activation, Dropout, BatchNormalization, LSTM
+from keras.layers import Bidirectional, Convolution1D, Dense, Activation, Dropout, BatchNormalization, LSTM, Flatten
 
 
-def lstm(window_size,
-        num_layers,
-        num_units,
-        filter_length,
-        dropout_prob,
-        num_epochs,
+def lstm(window_size=128,
+        num_layers=3,
+        num_units=1,
+        dropout_rate=0.3,
+        excludethresh=4.0,
+        num_epochs=3,
         dofft=False,
-        thesuffix='sliceres',
+        thesuffix='25.0Hz',
         modelname='model',
-        thedatadir='/data1/frederic/test/output'):
+        thedatadir='/data1/frederic/test/output',
+        readlim=None,
+        countlim=None):
+
     folder = './batch/'
-    #    train_x, train_y, val_x, val_y, Ns = dl.prep_ind(w)
-    print('cnn - loading data')
+    print('lstm - loading data')
     if dofft:
-        train_x, train_y, val_x, val_y, Ns, tclen, dummy, dummy = dl.prep(window_size, thesuffix=thesuffix, thedatadir=thedatadir, dofft=True)
+        train_x, train_y, val_x, val_y, Ns, tclen, thebatchsize, dummy, dummy = dl.prep(window_size,
+                                                                                      thesuffix=thesuffix,
+                                                                                      thedatadir=thedatadir,
+                                                                                      dofft=True,
+                                                                                      readlim=readlim,
+                                                                                      countlim=countlim)
     else:
-        train_x, train_y, val_x, val_y, Ns, tclen = dl.prep(window_size, thesuffix=thesuffix, thedatadir=thedatadir, dofft=False)
+        train_x, train_y, val_x, val_y, Ns, tclen, thebatchsize = dl.prep(window_size,
+                                                                          thesuffix=thesuffix,
+                                                                          thedatadir=thedatadir,
+                                                                          dofft=False,
+                                                                          readlim=readlim,
+                                                                          countlim=countlim)
+
+    print('dimension of input data', train_x.shape)
+    print('dimension of output data', train_y.shape)
     model = Sequential()
 
-    model.add(LSTM(num_units , activation='tanh', input_shape=(window_size, 1), recurrent_activation='hard_sigmoid'))
-    model.add(Dropout(rate=dropout_prob))
-    model.add(Dense(units=window_size, activation='linear'))
+    model.add(LSTM(num_units , activation='tanh', input_shape=(train_x.shape[1], train_x.shape[2],), recurrent_activation='hard_sigmoid'))
+    #model.add(Dropout(rate=dropout_rate))
+    #model.add(Flatten())
+    #model.add(Dense(window_size, input_shape=(train_x.shape[1],), activation='linear'))
     model.compile (loss ="mean_squared_error" , optimizer="adam")  
     history = model.fit(train_x, train_y,
-                        batch_size=1024,
+                        batch_size=train_x.shape[0],
                         epochs=num_epochs,
                         shuffle=False,
                         validation_data=(val_x, val_y))
@@ -62,7 +78,7 @@ def lstm(window_size,
     description = ' '.join([
         'Num layers: ', str(num_layers),
         'Num filters: ', str(num_filters),
-        'Dropout prob: ', str(dropout_prob),
+        'Dropout prob: ', str(dropout_rate),
         'Window size: ', str(window_size)
     ])
     print(description)
@@ -83,8 +99,8 @@ def lstm(window_size,
     plt.title('Training and validation loss')
     plt.legend()
     plt.savefig(
-        folder + 'loss' + '_layer_' + str(num_layers) + '_filter_num_' + str(num_filters) + '_dropout_prob_' + str(
-            dropout_prob) + '_window_size_' + str(window_size) + '.png')
+        folder + 'loss' + '_layer_' + str(num_layers) + '_filter_num_' + str(num_filters) + '_dropout_rate_' + str(
+            dropout_rate) + '_window_size_' + str(window_size) + '.png')
     plt.close()
 
     # print('loss, val_loss', loss, val_loss)
