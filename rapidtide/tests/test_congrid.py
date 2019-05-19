5a3cbea7a1ee8c1104d6ea83db55b827e5bf755c
Blaise Frederick <bbfrederick@mclean.harvard.edu>
2019-05-19 11:09:33 -0400

    Enhanced test_congrid, added more build targets

--- rapidtide/tests/test_congrid.py
+++ rapidtide/tests/test_congrid.py
@@ -7,22 +7,16 @@ import scipy as sp
 
 from rapidtide.resample import congrid
 from rapidtide.filter import dolpfiltfilt
-#from rapidtide.tests.utils import
+from rapidtide.tests.utils import mse
 
 import matplotlib.pyplot as plt
 
-
-def funcvalue(x, midpoint=500.0, width=25.0):
-    if midpoint - width <= x <= midpoint + width:
-        return 0.5
-    else:
-        return -0.5
 
 def funcvalue2(x, frequency=0.01, phase=0.0, amplitude=1.5):
     return amplitude * np.sin(2.0 * np.pi * frequency * x + phase)
 
 
-def test_congrid(debug=False):
+def test_congrid(debug=False, display=False):
     # make the source waveform
     tr = 1.0
     sourcelen = 1000
@@ -34,13 +28,6 @@ def test_congrid(debug=False):
     for i in range(len(sourceaxis)):
         timecoursein[i] = funcvalue2(sourceaxis[i])
 
-    '''
-    timecoursein = np.float64(sourceaxis * 0.0)
-    midpoint = int(sourcelen // 2) + 1
-    timecoursein[midpoint - 20:midpoint + 20] = np.float64(1.0)
-    timecoursein -= 0.5
-    '''
-
     # now make the destination
     gridlen = 150
     gridaxis = sp.linspace(0.0, tr * sourcelen, num=gridlen, endpoint=False)
@@ -51,57 +38,58 @@ def test_congrid(debug=False):
 
     # define the gridding
     congridbins = 1.5
-    gridkernel = 'gauss'
 
-    #butterorder = 4
-    #timecoursein = 0.5 * dolpfiltfilt(1.0, 0.25, timecoursein, butterorder) + 0.5
+    for gridkernel in ['gauss']:
+        print('about to grid')
 
-    print('about to grid')
-
-    numsamples = 5000
-    for i in range(numsamples):
-        t = np.random.uniform() * tr * sourcelen
-        thevals, theweights, theindices = congrid(gridaxis,
+        numsamples = 5000
+        for i in range(numsamples):
+            t = np.random.uniform() * tr * sourcelen
+            thevals, theweights, theindices = congrid(gridaxis,
                                                     t,
                                                     funcvalue2(t),
                                                     congridbins,
                                                     kernel=gridkernel,
                                                     debug=True)
-        for i in range(len(theindices)):
-            weights[theindices[i]] += theweights[i]
-            griddeddata[theindices[i]] += thevals[i]
+            for i in range(len(theindices)):
+                weights[theindices[i]] += theweights[i]
+                griddeddata[theindices[i]] += thevals[i]
 
-    griddeddata = np.where(weights > 0.0, griddeddata / weights, 0.0)
+        griddeddata = np.where(weights > 0.0, griddeddata / weights, 0.0)
 
-    print('gridding done')
-    print('debug:', debug)
+        target = np.float64(gridaxis * 0.0)
+        for i in range(len(gridaxis)):
+            target[i] = funcvalue2(gridaxis[i])
 
-    # plot if we are doing that
-    if debug:
-        offset = 0.0
-        legend = []
-        offset += 1.0
-        plt.plot(sourceaxis, timecoursein)
-        legend.append('Original')
-        offset += 1.0
-        plt.plot(gridaxis, griddeddata)
-        legend.append('Gridded')
-        plt.plot(gridaxis, weights)
-        legend.append('Weights')
+        print('gridding done')
+        print('debug:', debug)
 
-    # do the tests
-    #msethresh = 1e-6
-    #aethresh = 2
-    #assert mse(tcrolled, tcshifted) < msethresh
-    #np.testing.assert_almost_equal(tcrolled, tcshifted, aethresh)
+        # plot if we are doing that
+        if display:
+            offset = 0.0
+            legend = []
+            plt.plot(sourceaxis, timecoursein)
+            legend.append('Original')
+            offset += 1.0
+            plt.plot(gridaxis, target + offset)
+            legend.append('Target')
+            offset += 1.0
+            plt.plot(gridaxis, griddeddata + offset)
+            legend.append('Gridded')
+            plt.plot(gridaxis, weights)
+            legend.append('Weights')
+            plt.legend(legend)
+            plt.show()
 
-    if debug:
-        plt.legend(legend)
-        plt.show()
-
+        # do the tests
+        msethresh = 1e-3
+        themse = mse(target, griddeddata)
+        if debug:
+            print('mse:', themse)
+        assert themse < msethresh
 
 def main():
-    test_congrid(debug=True)
+    test_congrid(debug=True, display=True)
 
 
 if __name__ == '__main__':
