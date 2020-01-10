6cc7edad991b7f4315b5e5540cfeb9bdfa3a290d
Blaise Frederick <blaise.frederick@gmail.com>
2020-01-10 16:16:25 -0500

    Remove dead code

--- rapidtide/__init__.py
+++ rapidtide/__init__.py
@@ -1,9 +1,3 @@
 # -*- coding: latin-1 -*-
 from .version import __version__  # noqa
 from ._gittag import __gittag__
-
-#try:
-#    from .tidepoolTemplate_qt5 import *  # noqa
-#except:
-#    from .tidepoolTemplate_qt4 import *  # noqa
-#from .OrthoImageItem import *  # noqa
