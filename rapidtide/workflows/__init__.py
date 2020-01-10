f23dd9b8b3eed60c14eb2bfe29c1ccdbc3d8631a
Blaise Frederick <blaise.frederick@gmail.com>
2020-01-10 16:17:26 -0500

    Remove obsolete code

--- rapidtide/workflows/__init__.py
+++ rapidtide/workflows/__init__.py
@@ -1,10 +1,6 @@
-# emacs: -*- mode: python-mode; py-indent-offset: 4; tab-width: 4; -*-
 # ex: set sts=4 ts=4 sw=4 et:
 """
 Common rapidtide workflows.
 """
 
-from .rapidtide2 import rapidtide_workflow
-from .showxcorrx import showxcorrx_workflow
-
-__all__ = ['rapidtide_workflow', 'showxcorrx_workflow']
+from rapidtide.workflows.showxcorrx import showxcorrx_workflow
