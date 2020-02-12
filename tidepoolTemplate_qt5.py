18c385cfe2a71f7375aa7e471f2e6282254048e5
Blaise Frederick <blaise.frederick@gmail.com>
2020-02-11 20:05:01 -0500

    Added the initial regressor after filtering to the options in the regressor display

---
+++ tidepoolTemplate_qt5.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'tidepoolTemplate.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
