350539a42d98a5459ef769380d2ea93684a3456b
Blaise Frederick <blaise.frederick@gmail.com>
2020-03-20 17:21:49 -0400

    Added new way of selecting TkAgg

--- rapidtide/tests/test_timeshift.py
+++ rapidtide/tests/test_timeshift.py
@@ -18,6 +18,7 @@
 from __future__ import print_function, division
 
 import numpy as np
+import matplotlib
 import matplotlib.pyplot as plt
 
 from rapidtide.resample import timeshift
@@ -91,4 +92,5 @@ def main():
 
 
 if __name__ == '__main__':
+    matplotlib.use('TkAgg')
     main()
