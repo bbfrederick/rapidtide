37cb622c15405a7c5d77b5267b7c1d749e978c6e
Blaise Frederick <blaise.frederick@gmail.com>
2020-04-13 13:10:07 -0400

    Delete tidepoolTemplate_qt5.py

--- tidepoolTemplate_qt5.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'tidepoolTemplate.ui'
-#
-# Created by: PyQt5 UI code generator 5.12.3
-#
-# WARNING! All changes made in this file will be lost!
-
-
