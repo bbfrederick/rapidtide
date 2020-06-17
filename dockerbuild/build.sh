ed2d5f33ac9d376310752f6416933005028769ae
Blaise Frederick <blaise.frederick@gmail.com>
2020-06-17 19:51:55 -0400

    dev branch specific changes

--- dockerbuild/build.sh
+++ dockerbuild/build.sh
@@ -8,15 +8,9 @@ VERSION=`cat VERSION`
 echo "version: ${VERSION}"
 
 
-docker build . -t fredericklab/rapidtide:${VERSION} \
-    --build-arg VERSION=$version \
+docker build . -t fredericklab/rapidtide:${VERSION}-dev \
+    --build-arg VERSION=${VERSION}-dev \
     --build-arg BUILD_DATE=`date +"%Y%m%dT%k%M%S"` \
     --build-arg VCS_REF=`git rev-parse HEAD`
-docker build . -t fredericklab/rapidtide:latest \
-    --build-arg VERSION=$version \
-    --build-arg BUILD_DATE=`date +"%Y%m%dT%k%M%S"` \
-    --build-arg VCS_REF=`git rev-parse HEAD`
-docker push fredericklab/rapidtide:${VERSION}
-docker push fredericklab/rapidtide:latest
-docker pull fredericklab/rapidtide:${VERSION}
-docker pull fredericklab/rapidtide:latest
+docker push fredericklab/rapidtide:${VERSION}-dev
+docker pull fredericklab/rapidtide:${VERSION}-dev
