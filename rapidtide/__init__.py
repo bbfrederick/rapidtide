242bd2eafcb0bad789899457eb82fb35adfa66cd
Blaise Frederick <blaise.frederick@gmail.com>
2020-07-30 20:50:32 -0400

    Backporting changes to versioning

--- rapidtide/__init__.py
+++ rapidtide/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: latin-1 -*-
-from .version import __version__  # noqa
-from ._gittag import __gittag__
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
