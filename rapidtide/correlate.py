024869b161fe8cab31cd1755c71cd678b887723c
Taylor Salo <tsalo006@fiu.edu>
2021-03-27 15:52:00 -0400

    Remove unnecessary object.

--- rapidtide/correlate.py
+++ rapidtide/correlate.py
@@ -645,7 +645,7 @@ def cepstraldelay(data1, data2, timestep, displayplots=True):
     return timestep * np.argmax(residual_cepstrum.real[0 : len(residual_cepstrum) // 2])
 
 
-class aliasedcorrelator(object):
+class aliasedcorrelator:
     """An aliased correlator.
 
     Parameters
