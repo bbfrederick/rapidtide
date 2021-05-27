b69f61f834955ca8ecde32047e61e96ed2b09c9f
Blaise Frederick <blaise.frederick@gmail.com>
2021-05-26 22:01:28 -0400

    New docker diagnostic

---
+++ dockerbuild/inspect.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+MYIPADDRESS=`ifconfig en0 | grep 'inet ' | awk '{print $2}'`
+
+# allow network connections in Xquartz Security settings
+xhost +
+
+docker run \
+    --network host\
+    --volume=/Users/frederic:/data \
+    -it \
+    -e DISPLAY=${MYIPADDRESS}:0 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -u rapidtide fredericklab/rapidtide:1.9.4 \
+    xyzzy
