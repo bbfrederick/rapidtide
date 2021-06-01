f7a0f1483768bc6c1a2ec719d7956d41b7c92f24
Blaise Frederick <blaise.frederick@gmail.com>
2021-06-01 08:39:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -25,8 +25,11 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
 # Write version number out to VERSION file
 version = versioneer.get_version()
 date = versioneer.get_versions()["date"]
-with open(path.join(here, "VERSION"), "w", encoding="utf-8") as f:
-    f.write(version)
+try:
+    with open(path.join(here, "VERSION"), "w", encoding="utf-8") as f:
+        f.write(version)
+except PermissionError:
+    print("can't write to VERSION file - moving on")
 
 
 addtidepool = True
