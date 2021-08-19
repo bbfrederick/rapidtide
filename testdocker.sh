72cb563f61b58c4ef1954307f2bbf6b7e0992900
Blaise deB Frederick <blaise.frederick@gmail.com>
2021-08-19 14:54:47 -0400

    Update testdocker.sh

--- testdocker.sh
+++ testdocker.sh
@@ -5,19 +5,19 @@ MYIPADDRESS=`ifconfig en0 | grep 'inet ' | awk '{print $2}'`
 # allow network connections in Xquartz Security settings
 xhost +
 
-#docker run \
-    #--rm \
-    #--ipc host \
-    #--mount type=bind,source=/Users/frederic/code/rapidtide/rapidtide/data/examples,destination=/data \
-    #-it \
-    #-v /tmp/.X11-unix:/tmp/.X11-unix \
-    #-u rapidtide fredericklab/rapidtide:latest \
-    #rapidtide \
-        #/data/src/sub-RAPIDTIDETEST.nii.gz \
-        #/data/dst/sub-RAPIDTIDETEST \
-        #--passes 3 \
-        #--nprocs 4 \
-        #--noglm
+docker run \
+    --rm \
+    --ipc host \
+    --mount type=bind,source=/Users/frederic/code/rapidtide/rapidtide/data/examples,destination=/data \
+    -it \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -u rapidtide fredericklab/rapidtide:latest \
+    rapidtide \
+        /data/src/sub-RAPIDTIDETEST.nii.gz \
+        /data/dst/sub-RAPIDTIDETEST \
+        --passes 3 \
+        --nprocs 4 \
+        --noglm
 
 docker run \
     --rm \
