a186863663cba7b308ac0afbe6cbb1267b416114
Blaise deB Frederick <blaise.frederick@gmail.com>
2022-08-29 15:41:16 -0400

    Fixed versioneer

--- rapidtide/_version.py
+++ rapidtide/_version.py
@@ -43,7 +43,7 @@ def get_config():
     cfg = VersioneerConfig()
     cfg.VCS = "git"
     cfg.style = "pep440"
-    cfg.tag_prefix = ""
+    cfg.tag_prefix = "v"
     cfg.parentdir_prefix = ""
     cfg.versionfile_source = "rapidtide/_version.py"
     cfg.verbose = False
