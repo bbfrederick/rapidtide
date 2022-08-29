a696d4e48babd7cb026f9f554d922a32358059c8
Blaise deB Frederick <blaise.frederick@gmail.com>
2022-08-29 15:21:10 -0400

    Reinstalled versioneer

--- rapidtide/_version.py
+++ rapidtide/_version.py
@@ -44,7 +44,7 @@ def get_config():
     cfg.VCS = "git"
     cfg.style = "pep440"
     cfg.tag_prefix = ""
-    cfg.parentdir_prefix = "rapidtide-"
+    cfg.parentdir_prefix = ""
     cfg.versionfile_source = "rapidtide/_version.py"
     cfg.verbose = False
     return cfg
