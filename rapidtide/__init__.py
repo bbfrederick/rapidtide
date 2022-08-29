733a8fe25a2d77c952917eb64e3b3610ff31d373
Blaise deB Frederick <blaise.frederick@gmail.com>
2022-08-29 14:59:35 -0400

    Removed old versions

--- rapidtide/__init__.py
+++ rapidtide/__init__.py
@@ -1,12 +1,5 @@
 # Copyright (c) 2021, Blaise Frederick
 
 # -*- coding: latin-1 -*-
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
-
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
+from . import _version
+__version__ = _version.get_versions()['version']
