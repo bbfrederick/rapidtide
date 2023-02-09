177ae67afdc903954f1031e9e315928750347dc6
Blaise Frederick <blaise.frederick@gmail.com>
2023-02-09 13:39:53 -0500

    Updated versioneer

--- rapidtide/__init__.py
+++ rapidtide/__init__.py
@@ -1,6 +1,3 @@
-# Copyright (c) 2021, Blaise Frederick
-
 # -*- coding: latin-1 -*-
 from . import _version
-
-__version__ = _version.get_versions()["version"]
+__version__ = _version.get_versions()['version']
