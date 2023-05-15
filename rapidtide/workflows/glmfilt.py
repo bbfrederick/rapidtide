27a37a8cfef615ee310446d15f7a7a62eb9c0dc9
Blaise deB Frederick <blaise.frederick@gmail.com>
2023-05-15 17:33:03 -0400

    Gave the capabilities a major buff (added masking, output limits)

--- rapidtide/workflows/glmfilt.py
+++ rapidtide/workflows/glmfilt.py
@@ -32,7 +32,7 @@ def _get_parser():
     parser = argparse.ArgumentParser(
         prog="glmfilt",
         description="Fits and removes the effect of voxel specific and/or global regressors.",
-        usage="%(prog)s inputfile numskip outputroot --evfile FILE [FILE [FILE...]]",
+        usage="%(prog)s inputfile outputroot --numskip NUM --evfile FILE [FILE [FILE...]]",
     )
 
     # Required arguments
@@ -41,12 +41,14 @@ def _get_parser():
         type=lambda x: is_valid_file(parser, x),
         help="The name of the 3 or 4 dimensional nifti file to fit.",
     )
-    parser.add_argument(
-        "numskip",
-        type=int,
-        help="The number of points to skip at the beginning of the timecourse when fitting.",
-    )
     parser.add_argument("outputroot", type=str, help="The root name for all output files.")
+    parser.add_argument(
+        "--numskip",
+        dest="numskip",
+        type=int,
+        help="The number of points to skip at the beginning of the timecourse when fitting.  Default is 0.",
+        default=0,
+    )
     parser.add_argument(
         "--evfile",
         dest="evfile",
@@ -54,11 +56,26 @@ def _get_parser():
         nargs="+",
         help="One or more files (text timecourse or 4D NIFTI) containing signals to regress out.",
     )
+    parser.add_argument(
+        "--dmask",
+        dest="datamaskname",
+        type=lambda x: is_valid_file(parser, x),
+        action="store",
+        metavar="DATAMASK",
+        help=("Use DATAMASK to specify which voxels in the data to use."),
+        default=None,
+    )
+    parser.add_argument(
+        "--limitoutput",
+        dest="saveall",
+        action="store_false",
+        help=("Only save the filtered data and the R value."),
+        default=True,
+    )
     return parser
 
 
-def glmfilt(inputfile, numskip, outputroot, evfilename):
-
+def glmfilt(inputfile, numskip, outputroot, evfilename, datamaskname, saveall=True):
     # initialize some variables
     evdata = []
     evisnifti = []
@@ -72,6 +89,22 @@ def glmfilt(inputfile, numskip, outputroot, evfilename):
     print(xdim, ydim, slicedim, tr)
     xsize, ysize, numslices, timepoints = tide_io.parseniftidims(thedims_in)
     print(xsize, ysize, numslices, timepoints)
+
+    if datamaskname is not None:
+        print("reading in mask array")
+        (
+            datamask_img,
+            datamask_data,
+            datamask_hdr,
+            datamaskdims,
+            datamasksizes,
+        ) = tide_io.readfromnifti(datamaskname)
+        if not tide_io.checkspacematch(nim_header, datamask_hdr):
+            print("Data and mask dimensions do not match - exiting.")
+            sys.exit()
+        print("done reading in mask array")
+    else:
+        datamask_data = nim_data[:, :, :, 0] * 0.0 + 1.0
 
     numregressors = 0
     for i in range(0, len(evfilename)):
@@ -141,35 +174,37 @@ def glmfilt(inputfile, numskip, outputroot, evfilename):
         print("processing slice ", z)
         for y in range(0, ysize):
             for x in range(0, xsize):
-                regressorvec = []
-                for j in range(0, numregressors):
-                    if evisnifti[j]:
-                        regressorvec.append(evdata[j][x, y, z, :])
+                if datamask_data[x, y, z] > 0:
+                    regressorvec = []
+                    for j in range(0, numregressors):
+                        if evisnifti[j]:
+                            regressorvec.append(evdata[j][x, y, z, :])
+                        else:
+                            regressorvec.append(evdata[j])
+                    if np.max(trimmeddata[x, y, z, :]) - np.min(trimmeddata[x, y, z, :]) > 0.0:
+                        thefit, R = tide_fit.mlregress(regressorvec, trimmeddata[x, y, z, :])
+                        meandata[x, y, z] = thefit[0, 0]
+                        Rdata[x, y, z] = R
+                        for j in range(0, numregressors):
+                            fitdata[x, y, z, j] = thefit[0, j + 1]
+                            # datatoremove[x, y, z, :, j] = thefit[0, j + 1] * regressorvec[j]
                     else:
-                        regressorvec.append(evdata[j])
-                if np.max(trimmeddata[x, y, z, :]) - np.min(trimmeddata[x, y, z, :]) > 0.0:
-                    thefit, R = tide_fit.mlregress(regressorvec, trimmeddata[x, y, z, :])
-                    meandata[x, y, z] = thefit[0, 0]
-                    Rdata[x, y, z] = R
-                    for j in range(0, numregressors):
-                        fitdata[x, y, z, j] = thefit[0, j + 1]
-                        # datatoremove[x, y, z, :, j] = thefit[0, j + 1] * regressorvec[j]
-                else:
-                    meandata[x, y, z] = 0.0
-                    Rdata[x, y, z] = 0.0
-                    for j in range(0, numregressors):
-                        fitdata[x, y, z, j] = 0.0
-                        # datatoremove[x, y, z, :, j] = 0.0 * regressorvec[j]
-                # totaltoremove[x, y, z, :] = np.sum(datatoremove[x, y, z, :, :], axis=1)
-                # filtereddata[x, y, z, :] = trimmeddata[x, y, z, :] - totaltoremove[x, y, z, :]
+                        meandata[x, y, z] = 0.0
+                        Rdata[x, y, z] = 0.0
+                        for j in range(0, numregressors):
+                            fitdata[x, y, z, j] = 0.0
+                            # datatoremove[x, y, z, :, j] = 0.0 * regressorvec[j]
+                    # totaltoremove[x, y, z, :] = np.sum(datatoremove[x, y, z, :, :], axis=1)
+                    # filtereddata[x, y, z, :] = trimmeddata[x, y, z, :] - totaltoremove[x, y, z, :]
 
     # first save the things with a small numbers of timepoints
     print("fitting complete: about to save the fit data")
     theheader = nim_header
     theheader["dim"][4] = 1
-    tide_io.savetonifti(meandata, theheader, outputroot + "_mean")
-    for j in range(0, numregressors):
-        tide_io.savetonifti(fitdata[:, :, :, j], theheader, outputroot + "_fit" + str(j))
+    if saveall:
+        tide_io.savetonifti(meandata, theheader, outputroot + "_mean")
+        for j in range(0, numregressors):
+            tide_io.savetonifti(fitdata[:, :, :, j], theheader, outputroot + "_fit" + str(j))
     tide_io.savetonifti(Rdata, theheader, outputroot + "_R")
     Rdata = None
 
@@ -192,23 +227,31 @@ def glmfilt(inputfile, numskip, outputroot, evfilename):
     # now save the things with full timecourses
     theheader = nim_header
     theheader["dim"][4] = timepoints - numskip
-    tide_io.savetonifti(totaltoremove, theheader, outputroot + "_totaltoremove")
+    if saveall:
+        tide_io.savetonifti(totaltoremove, theheader, outputroot + "_totaltoremove")
     filtereddata = trimmeddata - totaltoremove
     totaltoremove = None
-    tide_io.savetonifti(trimmeddata, theheader, outputroot + "_trimmed")
+    if saveall:
+        tide_io.savetonifti(trimmeddata, theheader, outputroot + "_trimmed")
     trimmeddata = None
     tide_io.savetonifti(filtereddata, theheader, outputroot + "_filtered")
 
 
 def main():
-
     try:
         args = vars(_get_parser().parse_args())
     except SystemExit:
         _get_parser().print_help()
         raise
 
-    glmfilt(args["inputfile"], args["numskip"], args["outputroot"], args["evfile"])
+    glmfilt(
+        args["inputfile"],
+        args["numskip"],
+        args["outputroot"],
+        args["evfile"],
+        args["datamaskname"],
+        saveall=args["saveall"],
+    )
 
 
 if __name__ == "__main__":
