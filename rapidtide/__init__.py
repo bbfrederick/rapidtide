cf5c2b12db0280915be3a62fd3d51593b728eea6
Blaise deB Frederick <blaise.frederick@gmail.com>
2023-10-05 10:53:20 -0400

    Installed versioneer in vendor mode

--- rapidtide/__init__.py
+++ rapidtide/__init__.py
@@ -6,3 +6,6 @@ __version__ = _version.get_versions()["version"]
 from . import _version
 
 __version__ = _version.get_versions()["version"]
+
+from . import _version
+__version__ = _version.get_versions()['version']
