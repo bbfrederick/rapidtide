aaa4c27c632429b95fa22cc9ecc78a2116f315ab
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-01-29 14:38:23 -0500

    Update RapidtideDataset.py

--- rapidtide/RapidtideDataset.py
+++ rapidtide/RapidtideDataset.py
@@ -274,7 +274,8 @@ class Overlay:
             else:
                 tempmask = (0 * self.data).astype("uint16")
                 for theval in self.filevals:
-                    tempmask[np.where(self.data - theval == 0)] += 1
+                    print(f"looking for {theval}")
+                    tempmask[np.where(self.data - 1.0 * theval == 0.0)] += 1
                 self.data = np.where(tempmask > 0, 1, 0)
         if self.verbose > 1:
             print("Overlay data range:", np.min(self.data), np.max(self.data))
@@ -833,7 +834,6 @@ class RapidtideDataset:
             return False
 
     def _loadgraymask(self):
-        print(f"{self.graymaskspec=}")
         if self.graymaskspec is not None:
             filename, dummy = tide_io.parsefilespec(self.graymaskspec)
             if os.path.isfile(filename):
