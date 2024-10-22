cc95867c4cf0f9a53040aee11b9f2161e2bc2c09
Taylor Salo <salot@pennmedicine.upenn.edu>
2024-10-22 13:26:41 -0400

    Fix thing.

--- docs/conf.py
+++ docs/conf.py
@@ -93,12 +93,7 @@ templates_path = ["_templates"]
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-source_parsers = {
-    ".md": CommonMarkParser,
-}
-
-source_suffix = [".rst", ".md"]
-
+source_suffix = [".rst"]
 
 # The encoding of source files.
 # source_encoding = 'utf-8-sig'
