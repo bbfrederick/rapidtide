b743321c3afed619808fce7bd7ed0d2d5adf8505
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-11-19 16:04:32 -0500

    Tidy up, add explicit container pull

--- dockershell.sh
+++ dockershell.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker run -it fredericklab/rapidtide:latest bash
+VERSION=latest
+
+docker pull fredericklab/rapidtide:${VERSION}
+docker run -it fredericklab/rapidtide:${VERSION} bash
