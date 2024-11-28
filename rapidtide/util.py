d42a752badb8ae645e9c7f79f978cc581d688570
Blaise deB Frederick <blaise.frederick@gmail.com>
2024-11-27 20:47:45 -0500

    Unlinking shm immediately after creating sharedmemory object

--- rapidtide/util.py
+++ rapidtide/util.py
@@ -1018,6 +1018,7 @@ def numpy2shared(inarray, theouttype, name=None):
     # Create a shared memory block to store the array data
     outnbytes = np.dtype(theouttype).itemsize * inarray.size
     shm = shared_memory.SharedMemory(name=None, create=True, size=outnbytes)
+    shm.unlink()
     inarray_shared = np.ndarray(inarray.shape, dtype=theouttype, buffer=shm.buf)
     np.copyto(inarray_shared, inarray)  # Copy data to shared memory array
     return inarray_shared, shm  # Return both the array and the shared memory object
@@ -1030,12 +1031,14 @@ def allocshared(theshape, thetype, name=None):
     dtype_size = np.dtype(thetype).itemsize
     # Create a shared memory block of the required size
     shm = shared_memory.SharedMemory(name=None, create=True, size=thesize * dtype_size)
+    shm.unlink()
     outarray = np.ndarray(theshape, dtype=thetype, buffer=shm.buf)
     return outarray, shm  # Return both the array and the shared memory object
 
 
 def cleanup_shm(shm):
     # Cleanup
-    if shm is not None:
-        shm.close()
-        shm.unlink()
+    pass
+    # if shm is not None:
+    #    shm.close()
+    #    shm.unlink()
