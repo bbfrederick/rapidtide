bcf941f052474114a1e10c169fec72bb15cc56e1
Blaise deB Frederick <blaise.frederick@gmail.com>
2025-01-17 10:05:07 -0500

    Update RapidtideDataset.py

--- rapidtide/RapidtideDataset.py
+++ rapidtide/RapidtideDataset.py
@@ -229,7 +229,7 @@ class Overlay:
             label=newlabel,
             report=self.report,
             init_LUT=True,
-            lut_state=self.lut_state,
+            lut_state=gen_gray_state(),
         )
 
     def updateStats(self):
