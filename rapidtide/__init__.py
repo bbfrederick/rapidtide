67da6dee572eeae5eea22bc8d00d3591eed4a196
Blaise deB Frederick <blaise.frederick@gmail.com>
2026-02-17 17:08:01 -0500

    Attempt to fix the problem in running codex tests

--- rapidtide/__init__.py
+++ rapidtide/__init__.py
@@ -1,4 +1,30 @@
 # -*- coding: utf-8 -*-
+import os
+import tempfile
+from pathlib import Path
+
 from . import _version
+
+
+def _configure_matplotlib_cache():
+    if "MPLCONFIGDIR" in os.environ:
+        return
+
+    default_mpl_dir = Path.home() / ".matplotlib"
+    try:
+        if default_mpl_dir.exists():
+            if os.access(default_mpl_dir, os.W_OK | os.X_OK):
+                return
+        elif os.access(default_mpl_dir.parent, os.W_OK | os.X_OK):
+            return
+    except OSError:
+        pass
+
+    fallback_dir = Path(tempfile.gettempdir()) / "rapidtide-mplconfig"
+    fallback_dir.mkdir(parents=True, exist_ok=True)
+    os.environ["MPLCONFIGDIR"] = str(fallback_dir)
+
+
+_configure_matplotlib_cache()
 
 __version__ = _version.get_versions()["version"]
--- rapidtide/tests/usercustomize.py
+++ rapidtide/tests/usercustomize.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python
 
-print("invoking coverage.process_startup()")
-try:
-    import coverage
-except ImportError:
-    pass
-else:
-    coverage.process_startup()
+import sitecustomize  # noqa: F401
