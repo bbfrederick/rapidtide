5572b20ace8d93720e7b534acc2efc789f3cdddc
Blaise deB Frederick <blaise.frederick@gmail.com>
2026-04-07 12:31:08 -0400

    Added some options, format output file more usefully

--- rapidtide/workflows/tp2rt.py
+++ rapidtide/workflows/tp2rt.py
@@ -20,9 +20,9 @@ import argparse
 from typing import Any
 
 import cv2
-import nibabel as nib
 import numpy as np
-from numpy.typing import NDArray
+
+import rapidtide.io as tide_io
 
 
 def _get_parser() -> Any:
@@ -33,14 +33,18 @@ def _get_parser() -> Any:
         allow_abbrev=False,
     )
     parser.add_argument("infilename", help="An mp4 file output by TelePlethy")
-    parser.add_argument("outfilename", help="Root file for NIFTI files")
+    parser.add_argument("outfileroot", help="Root file for NIFTI files")
+
+    parser.add_argument(
+        "--framerate", type=float, default=30.0, help="The frame rate of the input video"
+    )
 
     return parser
 
 
 def main(args):
     # 1. Open the video file using OpenCV
-    cap = cv2.VideoCapture(args.inputfile)
+    cap = cv2.VideoCapture(args.infilename)
     frames = []
 
     while cap.isOpened():
@@ -49,22 +53,30 @@ def main(args):
             break
 
         # 2. Convert frame to r, g and b
-        # r_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        g_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        # b_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frames.append(g_frame)
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        g_frame = rgb_frame[::-1, :, 1]
+        frames.append(np.transpose(g_frame))
 
     cap.release()
 
-    # 3. Stack frames into a 3D numpy array (Depth, Height, Width)
+    # 3. Stack frames into a 3D numpy array (Height, Width, time)
     # Shape will be (NHeight, Width, Number of frames)
-    data_array = np.stack(frames, axis=2)
+    init_array = np.stack(frames, axis=2)
+    initshape = init_array.shape
+    print(f"{init_array.shape=}")
+    data_array = init_array.reshape(initshape[0], 1, initshape[1], initshape[2])
+    print(f"{data_array.shape=}")
 
     # 4. Create a NIfTI image using NiBabel
     # We use an identity matrix for the affine as MP4 lacks spatial metadata
-    affine = np.eye(4)
-    nifti_img = nib.Nifti1Image(data_array, affine)
+    thehdr = tide_io.niftihdrfromarray(data_array)
+    thehdr.set_xyzt_units(xyz="mm", t="sec")
+    thehdr["pixdim"][4] = 1.0 / args.framerate
+    tide_io.savetonifti(data_array, thehdr, args.outfileroot)
+
+    # affine = np.eye(4)
+    # nifti_img = nib.Nifti1Image(data_array, affine)
 
     # 5. Save the NIfTI file
-    nib.save(nifti_img, args.outputfile)
-    print(f"Successfully converted {args.inputfile} to {args.outputfile}")
+    # nib.save(nifti_img, args.outfileroot)
+    print(f"Successfully converted {args.infilename} to {args.outfileroot}")
